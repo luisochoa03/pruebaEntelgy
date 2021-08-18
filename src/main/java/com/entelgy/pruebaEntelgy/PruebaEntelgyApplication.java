3dc256a34e9e553640d02d9b48940ca3737417c1
Luis Ochoa <lr.ochoav@gmail.com>
2021-08-18 17:48:23 -0500

    initializer base de Spring Boot

---
+++ src/main/java/com/entelgy/pruebaEntelgy/PruebaEntelgyApplication.java
@@ -0,0 +1,13 @@
+package com.entelgy.pruebaEntelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaEntelgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaEntelgyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entelgy/pruebaEntelgy/PruebaEntelgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entelgy.pruebaEntelgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaEntelgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
